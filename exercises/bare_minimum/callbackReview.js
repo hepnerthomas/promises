b461e814e241d1f45d0a79e2e864fb46fd0ae57e
Thomas Hepner <hepner.thomas@gmail.com>
2022-08-09 15:14:37 -0700

    feat: add getStatusCode function for callbackReview.js with all tests passing.

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -8,32 +8,25 @@ var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFile = function (filePath, callback) {
-  // TODO
   fs.readFile(filePath, 'utf-8', (err, fileData) => {
     if (err) {
-      // throw ('could not read the file data.');
       callback(err, null);
     } else {
-      // console.log(fileData);
-      // console.log(fileData[0].toString());
       let lineArray = fileData.split('\n');
-      // console.log(lineArray[0]);
       callback(null, lineArray[0]);
-      // callback(err, null)
-      // console.log(fileData);
     }
-
-
-  // const readLine = readline.createInterface({input: filePath})
   });
-  // readLine.close();
-
-
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request('get', url, (err, resp) => {
+    if (err) {
+        callback(err, null);
+    } else {
+      callback(err, resp.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
